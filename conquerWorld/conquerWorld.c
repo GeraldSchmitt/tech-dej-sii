007caebe3e16b918334fb053eb57535d60c71015
Gerald <schmittgerald@gmail.com>
2018-05-02 08:14:01 +0200

    Library actually uses c-utility

--- conquerWorld/conquerWorld.c
+++ conquerWorld/conquerWorld.c
@@ -1,8 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "conquerWorld.h"
-#include "httpapi.h"
+#include "azure_c_shared_utility/httpapi.h"
 
 int frobnicate()
 {
 	HTTP_HANDLE handle;
+	HTTPAPI_RESULT res = HTTPAPI_Init();
+	if (res == HTTPAPI_OK)
+	{
+		printf("Init success!\n");
+		
+		HTTPAPI_Deinit();
+	}
+	else
+	{
+		printf("Init fail...\n");
+	}
 	return 43;
 }
